64c499a514e4de2439baf5a5038ea9b662a834db
John Alvaro <jhon54plex@hotmail.com>
2018-08-08 11:58:23 -0500

    Prueba de cron3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('noti:notificaciones')->cron('* * * * *');
+        $schedule->command('noti:notificaciones')->everyMinute();
     }
 
     /**
