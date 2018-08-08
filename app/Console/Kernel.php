8014538dca3d18c060e2f0297a5c6572442a5ea4
John Alvaro <jhon54plex@hotmail.com>
2018-08-08 12:12:34 -0500

    Cancelando cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('noti:notificaciones')->everyMinute();
+        // $schedule->command('noti:notificaciones')->everyMinute();
     }
 
     /**
