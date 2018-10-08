4abce56ed9943832021b50bf05918e84ac7a4103
John Alvaro <jhon54plex@hotmail.com>
2018-10-08 12:10:15 -0500

    Agregando script para form de tratamiento de datos

--- app/Http/Controllers/WebserviceController.php
+++ app/Http/Controllers/WebserviceController.php
@@ -11,7 +11,7 @@ class WebserviceController extends Controller
 
     public function create(Request $request){
         $persona = new Persona();
-        return 'Recibido desde ixtus';
+        return $request;
     }
 
 
