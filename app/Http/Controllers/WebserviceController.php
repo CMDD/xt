3fdfdc60c79beb02be16a22bcb61c20775591b2a
John Alvaro <jhon54plex@hotmail.com>
2018-10-08 15:28:59 -0500

    Primeras pruebas para werbservices

--- app/Http/Controllers/WebserviceController.php
+++ app/Http/Controllers/WebserviceController.php
@@ -10,8 +10,34 @@ class WebserviceController extends Controller
 
 
     public function create(Request $request){
-        $persona = new Persona();
-        return $request;
+
+      $persona = Persona::where('numero_documento',$request->cedula)->first();
+
+      if ($persona) {
+        $persona->estado = 'Activo';
+        $persona->nombres = $request->nombres;
+        $persona->apellidos = $request->apellidos;
+        $persona->correo = $request->email;
+        $persona->user_id = 1;
+        $persona->numero_planilla = $request->ip;
+        $persona->numero_registro ='Web IP: ' ;
+        $persona->imagen = 'web/formato.pdf';
+        $persona->save();
+      }else{
+      $persona = new Persona();
+      $persona->estado = 'Activo';
+      $persona->nombres = $request->nombres;
+      $persona->apellidos = $request->apellidos;
+      $persona->numero_documento = $request->cedula;
+      $persona->correo = $request->email;
+      $persona->user_id = 1;
+      $persona->numero_planilla = $request->ip;
+      $persona->numero_registro ='Web IP: ' ;
+      $persona->imagen = 'web/formato.pdf';
+      $persona->save();
+
+        }
+      return back();
     }
 
 
