ccefae62fa359da40f96d566e8ceef8100798f41
John Alvaro <jhon54plex@hotmail.com>
2018-11-02 13:19:55 -0500

    Configurando servicio web

--- app/Http/Controllers/WebserviceController.php
+++ app/Http/Controllers/WebserviceController.php
@@ -16,19 +16,22 @@ class WebserviceController extends Controller
       $persona = Persona::where('numero_documento',$request->cedula)->first();
 
       if ($persona) {
-        if ($persona->estado == 'Activo') {
 
-        }else{
-          $persona->estado = 'Activo';
-          $persona->nombres = $request->nombres;
-          $persona->apellidos = $request->apellidos;
-          $persona->correo = $request->email;
-          $persona->user_id = 1;
-          $persona->numero_planilla = $request->ip;
-          $persona->numero_registro ='Web IP: ' ;
-          $persona->imagen = 'web/formato.pdf';
-          $persona->save();
-        }
+        // if ($persona->estado == 'Activo') {
+        //
+        //   return back();
+        // }else{
+        //   $persona->estado = 'Activo';
+        //   $persona->nombres = $request->nombres;
+        //   $persona->apellidos = $request->apellidos;
+        //   $persona->correo = $request->email;
+        //   $persona->user_id = 1;
+        //   $persona->numero_planilla = $request->ip;
+        //   $persona->numero_registro ='Web IP: ' ;
+        //   $persona->imagen = 'web/formato.pdf';
+        //   $persona->save();
+        // }
+        dd($persona);
 
       }else{
       $persona = new Persona();
