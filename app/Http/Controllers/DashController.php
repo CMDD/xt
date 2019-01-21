46e76293122966abe1ee0bd417dced8719367e8e
John Alvaro <jhon54plex@hotmail.com>
2019-01-21 10:23:40 -0500

    Reporte cantidades dashboard

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -29,18 +29,71 @@ class DashController extends Controller
 
         //Valores nacionales Titulares
       $total = Persona::count();
-      $titular_activo = Persona::where('estado','Activo')->count();
+      $personas = Persona::where('estado','Activo')->get();
       $titular_desactivo = Persona::where('estado','Desactivo')->count();
+      $titular_activo = $personas->count();
 
-      $oyente = TipoPersona::where('nombre','Oyente')->count();
-      $cliente = TipoPersona::where('nombre','Cliente')->count();
-      $alumno = TipoPersona::where('nombre','Alumno')->count();
-      $asistente = TipoPersona::where('nombre','Asistente')->count();
-      $servidor = TipoPersona::where('nombre','Servidor')->count();
-      $proveedor = TipoPersona::where('nombre','Proveedor')->count();
-      $suscriptor = TipoPersona::where('nombre','Suscriptor')->count();
-      $benefactor = TipoPersona::where('nombre','Benefactor')->count();
-      $empleado = TipoPersona::where('nombre','Empleado')->count();
+      $oyente=0;
+      $cliente=0;
+      $alumno =0;
+      $asistente=0;
+      $servidor=0;
+      $proveedor=0;
+      $suscriptor=0;
+      $benefactor=0;
+      $empleado=0;
+
+
+      foreach($personas as $to){
+        $oyentes = TipoPersona::where('nombre','Oyente')->where('persona_id',$to->id)->count();
+        $clientes = TipoPersona::where('nombre','Cliente')->where('persona_id',$to->id)->count();
+        $alumnos = TipoPersona::where('nombre','Alumno')->where('persona_id',$to->id)->count();
+        $asistentes = TipoPersona::where('nombre','Asistente')->where('persona_id',$to->id)->count();
+        $servidores = TipoPersona::where('nombre','Servidor')->where('persona_id',$to->id)->count();
+        $proveedores = TipoPersona::where('nombre','Proveedor')->where('persona_id',$to->id)->count();
+        $suscriptores  = TipoPersona::where('nombre','Suscriptor')->where('persona_id',$to->id)->count();
+        $benefactores  = TipoPersona::where('nombre','Benefactor')->where('persona_id',$to->id)->count();
+        $empleados  = TipoPersona::where('nombre','Empleado')->where('persona_id',$to->id)->count();
+        if ($oyentes > 0) {
+          $oyente = $oyente +1;
+        }
+        if ($clientes > 0) {
+          $cliente = $cliente +1;
+        }
+        if ($alumnos > 0) {
+          $alumno = $alumno +1;
+        }
+        if ($asistentes > 0) {
+          $asistente =$asistente +1;
+        }
+        if ($servidores > 0) {
+          $servidor =  $servidor +1;
+        }
+        if ($proveedores > 0) {
+          $proveedor =  $proveedor +1;
+        }
+        if ($suscriptores  > 0) {
+          $suscriptor = $suscriptor +1;
+        }
+        if ($benefactores  > 0) {
+          $benefactor = $benefactor +1;
+        }
+        if ($empleados  > 0) {
+          $empleado = $empleado +1;
+        }
+      }
+
+
+
+      // $oyente = TipoPersona::where('nombre','Oyente')->count();
+      // $cliente = TipoPersona::where('nombre','Cliente')->count();
+      // $alumno = TipoPersona::where('nombre','Alumno')->count();
+      // $asistente = TipoPersona::where('nombre','Asistente')->count();
+      // $servidor = TipoPersona::where('nombre','Servidor')->count();
+      // $proveedor = TipoPersona::where('nombre','Proveedor')->count();
+      // $suscriptor = TipoPersona::where('nombre','Suscriptor')->count();
+      // $benefactor = TipoPersona::where('nombre','Benefactor')->count();
+      // $empleado = TipoPersona::where('nombre','Empleado')->count();
 
       $suscripciones = Suscripcion::count();
       $sus_activo= Suscripcion::where('estado','Activo')->count();
