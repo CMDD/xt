337057bcb68a607f24409e8b605862f6cb7965b6
John Alvaro <jhon54plex@hotmail.com>
2019-02-16 10:23:15 -0500

    Agregando fecha apartir de

--- resources/views/admin/reporte/suscripciones.blade.php
+++ resources/views/admin/reporte/suscripciones.blade.php
@@ -45,7 +45,7 @@ ACTIVO
 </td>
 <td class=" ">{{$p->plan}} Meses</td>
 <td class=" ">{{$p->fecha_inicio->format('d-m-y')}}</td>
-<td class=" "></td>
+<td class=" ">{{$p->apartir_de->format('d-m-y')}}</td>
 <td class="a-right a-right "></td>
 <td class="a-right a-right "></td>
 <td class="a-right a-right "></td>
