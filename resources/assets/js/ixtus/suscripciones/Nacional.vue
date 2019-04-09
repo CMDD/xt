01f78c6f7302d45ee56d0462b91be9adfd3465a6
John Alvaro <jhon54plex@hotmail.com>
2019-04-08 19:20:49 -0500

    Corigiendo ajax

--- resources/assets/js/ixtus/suscripciones/Nacional.vue
+++ resources/assets/js/ixtus/suscripciones/Nacional.vue
@@ -62,7 +62,7 @@ export default {
         $('#datatable-suscripciones').DataTable({
           "serverSide":true,
           type: "get",
-          "ajax":'/api/suscripcioes/nacional',
+          "ajax":'sus/cacional',
           "columns":[
             {data:'estado'},
             {data:'plan'},
