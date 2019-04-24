2be86c8e7ecc2e247780ad418fd1ea9efba262a6
John Alvaro <jhon54plex@hotmail.com>
2019-04-24 14:26:32 -0500

    Corrigiendo fechas

--- app/Http/Controllers/SuscripcionController.php
+++ app/Http/Controllers/SuscripcionController.php
@@ -109,7 +109,7 @@ class SuscripcionController extends Controller
        $sus->numero_factura = $request->numero_factura;
        $sus->punto_venta = $request->punto;
        $sus->apartir_de = $apartir_de;
-       $sus->envio_hasta = $apartir_de->addMonths((int)$request->tiempo - 1);
+       $sus->envio_hasta = $apartir_de->addMonths((int)$request->tiempo);
        $sus->tipo = 'Nueva';
        $sus->save();
      }else{
@@ -149,7 +149,7 @@ class SuscripcionController extends Controller
        $sus->numero_factura = $request->numero_factura;
        $sus->punto_venta = $request->punto;
        $sus->apartir_de = $apartir_de;
-       $sus->envio_hasta = $apartir_de->addMonths((int)$request->tiempo - 1);
+       $sus->envio_hasta = $apartir_de->addMonths((int)$request->tiempo);
        $sus->tipo = 'Nueva';
 
 
