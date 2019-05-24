d8137bf6785e329f0095fe53e3efe6f7f1df44db
John Alvaro <jhon54plex@hotmail.com>
2019-05-24 10:46:51 -0500

    Agregando if a fecha apartir_de

--- resources/views/admin/suscripcion/editar.blade.php
+++ resources/views/admin/suscripcion/editar.blade.php
@@ -169,10 +169,10 @@
  <div class="col-md-6 form-group">
    <label for="">Fecha de inicio</label>
    @if($sus->apartir_de)
-  <input type="date" class="form-control" id="" name="apartir_de" value=""
+  <input type="date" class="form-control" id="" name="apartir_de" value="{{$sus->apartir_de->format('Y-m-d')}}"
     name="apartir_de">
   @else
-   <input type="date" class="form-control" id="" name="apartir_de" value="{{$sus->apartir_de->format('Y-m-d')}}"
+   <input type="date" class="form-control" id="" name="apartir_de" value=""
   name="apartir_de">
   @endif
 
